7d62d74f105eb1535d7991796ccba251fbbd2bee
Miguel <mederosmiguel99@gmail.com>
2023-01-09 13:20:44 -0500

    valiadate embarazada

--- controllers/embarazada.js
+++ controllers/embarazada.js
@@ -12,19 +12,25 @@ module.exports = {
     },
     create: async (req, res) => {
         const { Carnet_Identidad, Nombre, Telefono, Provincia, Municipio, Direccion, Area_Salud, Codigo_ingreso, NoHC, NoConsulotorio } = req.body
-        const embarazada = await EmbarazadasModel.create({
-            Carnet_Identidad,
-            Nombre,
-            Telefono,
-            Provincia,
-            Municipio,
-            Direccion,
-            Area_Salud,
-            Codigo_ingreso,
-            NoHC,
-            NoConsulotorio
-        })
-        res.status(200).json(embarazada)
+        let embarazada = await EmbarazadasModel.findOne({Carnet_Identidad})
+        if(!embarazada){
+            embarazada = await EmbarazadasModel.create({
+                Carnet_Identidad,
+                Nombre,
+                Telefono,
+                Provincia,
+                Municipio,
+                Direccion,
+                Area_Salud,
+                Codigo_ingreso,
+                NoHC,
+                NoConsulotorio
+            })
+            res.status(200).json(embarazada)
+        }else{
+            res.status(200).json({error:"Embarazada existente"})
+        }
+        
     },
     update: async (req, res) => {
         const { id, Carnet_Identidad, Nombre, Telefono, Provincia, Municipio, Direccion, Area_Salud, Codigo_ingreso, NoHC, NoConsulotorio} = req.body
